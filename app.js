442f4b4a4f81f000972f610a3d59d565b92c67af
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-31 17:00:17 -0500

    basic site complete

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/views/about.html");
+});
+app.get("/works", function (req, res) {
+  res.sendFile(__dirname + "/views/works.html");
+});
+app.get("/photos", function (req, res) {
+  res.sendFile(__dirname + "/views/photos.html");
+});
+
+app.listen(3000, function () {
+  console.log("We are now listening on port 3000");
+});
